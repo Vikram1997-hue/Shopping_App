71e912e9bea50664364128df802d2aeb523631f3
Vikram1997-hue <ranterdudewtd@gmail.com>
2022-04-21 13:16:18 +0530

    testing push

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Shopping App',
+      title: 'Shopping Apppp',
 
       theme: ThemeData(
         primarySwatch: Colors.blue,
