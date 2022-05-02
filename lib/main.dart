4d0ac35efe8e07a21370c45b6010674a50c1fabe
Vikramaditya Bhatnagar <55340732+Vikram1997-hue@users.noreply.github.com>
2022-05-02 20:12:05 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:untitled/constants/constants.dart';
+import 'package:untitled/screens/auth/login.dart';
 import 'package:untitled/screens/home/home_screen.dart';
 import 'package:untitled/splash_screen.dart';
 import 'package:untitled/theme.dart';
-
+//import 'package:razorpay_flutter/razorpay_flutter.dart';
 
 
 void main() {
@@ -13,14 +14,20 @@ void main() {
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
 
+
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Shopping App',
-      home: HomeScreen(), //SplashScreen(), //HomeScreen(),
+      home: SplashScreen(), //HomeScreen(),
       theme: AppTheme(context).customTheme,
+
+      routes: {
+        '/home': (context) => HomeScreen(),
+      },
     );
   }
 }
-
