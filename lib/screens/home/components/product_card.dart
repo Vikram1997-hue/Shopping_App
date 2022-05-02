948447f01c4a802afac107f5fb6ff7b63a29d2df
Vikramaditya Bhatnagar <55340732+Vikram1997-hue@users.noreply.github.com>
2022-05-02 19:58:24 +0530

    Update product_card.dart

--- lib/screens/home/components/product_card.dart
+++ lib/screens/home/components/product_card.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
 import 'package:untitled/constants/constants.dart';
 
-class NewArrivalsProduct extends StatelessWidget {
-  const NewArrivalsProduct({
+class ProductCard extends StatelessWidget {
+  const ProductCard({
     Key? key,
     required this.image,
     required this.title,
     required this.color,
     required this.price,
     required this.press,
+
+    this.hasFavourite = false,
+    this.isFavourite = false,
+    this.onFavouriteClicked,
   }) : super(key: key);
 
   final String image, title;
@@ -16,12 +21,17 @@ class NewArrivalsProduct extends StatelessWidget {
   final int price;
   final VoidCallback press;
 
+  //these ones are only used in search results
+  final bool hasFavourite;
+  final bool isFavourite;
+  final VoidCallback? onFavouriteClicked;
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: press,
       child: Container(
-        width: 154,
+        width: hasFavourite ? MediaQuery.of(context).size.width : 154, //MAKE THIS A ??
         padding: EdgeInsets.all(defaultPadding/2),
         decoration: BoxDecoration(
           color: Colors.white,
@@ -29,16 +39,35 @@ class NewArrivalsProduct extends StatelessWidget {
         ),
         child: Column(
           children: [
-            Container(
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: color,
-                borderRadius: BorderRadius.circular(defaultBorderRadius),
-              ),
-              child: Image.asset(
-                image,
-                height: 132,
-              ),
+            Stack(
+              children: [
+                Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: color,
+                    borderRadius: BorderRadius.circular(defaultBorderRadius),
+                  ),
+                  child: Image.asset(
+                    image,
+                    height: 132,
+                  ),
+                ),
+
+                if(hasFavourite)
+                Positioned(
+                  top: 10,
+                  right: 10,
+                  child: CircleAvatar(
+                    backgroundColor: Colors.white,
+                    radius: 15,
+                    child: Icon(
+                      isFavourite ? IconlyBold.heart : IconlyLight.heart,
+                      size: 21,
+                      color: Colors.red,
+                    ),
+                  ),
+                ),
+              ],
             ),
             SizedBox(height: defaultPadding/2.2,),
             Row(
